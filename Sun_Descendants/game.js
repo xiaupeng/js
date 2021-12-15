5a4fd722fb368451703651132568a832be16e298
xiaupeng <86650877+xiaupeng@users.noreply.github.com>
2021-12-15 14:00:48 +0800

    Add files via upload

---
+++ Sun_Descendants/game.js
@@ -0,0 +1,28 @@
+﻿var game;
+window.onload=function()
+{
+var config = {
+    type: Phaser.AUTO,
+    // pixel size * tile map size * zoom 
+    width: 32 * 20,
+    height: 32 * 20,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+
+    backgroundColor: '#000000',
+    pixelArt: true,
+    scene: [main,world, room1, room2,room3, room4,winScene]
+};
+
+game = new Phaser.Game(config);
+window.elixir = 0;
+
+}
